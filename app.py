e623e60d0ecc491ffabb358833b5ef4051d883ac
Andi Muchammad Fadel Abista <163365121+andeemfdel@users.noreply.github.com>
2024-10-04 13:45:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ Original file is located at
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sb
 import datetime as dt
 
 """## Data Wrangling
